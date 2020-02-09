7920455efe9f70b2eb210001f012eb8b6d768744
sitais <daniel.levy@sitais.com>
2020-02-09 12:21:16 +0000

    ui sections
    Broken down UI into sections

--- lib/home.dart
+++ lib/home.dart
@@ -3,33 +3,74 @@ import 'package:flutter/material.dart';
 /// Empty page widget fo developers that want to follow along with the tutorial
 /// on breaking down and implementing the Tik Tok UI (BLOG_LINK)
 class Home extends StatelessWidget {
+  Widget get topSection => Container(
+        height: 100.0,
+        padding: EdgeInsets.only(bottom: 15.0),
+        color: Colors.yellow[300],
+      );
+
+  Widget get videoDescription => Expanded(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  height: 10.0,
+                  color: Colors.green[300],
+                  margin: EdgeInsets.only(top: 10)),
+              Container(
+                  height: 10.0,
+                  color: Colors.green[300],
+                  margin: EdgeInsets.only(top: 10)),
+              Container(
+                  height: 10.0,
+                  color: Colors.green[300],
+                  margin: EdgeInsets.only(top: 10))
+            ]),
+      );
+
+  Widget get actionsToolbar => Container(
+        width: 100.0,
+        color: Colors.red[300],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: List<Widget>.generate(
+              5,
+              (index) => Container(
+                  width: 60,
+                  height: 60,
+                  color: Colors.blue[300],
+                  margin: EdgeInsets.only(top: 20.0))),
+        ),
+      );
+
+  Widget get middleSection => Expanded(
+      child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[videoDescription, actionsToolbar]));
+
+  Widget get bottomSection => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List<Widget>.generate(
+            5,
+            (index) => Container(
+                width: 40.0, height: 40.0, color: Colors.purple[300])),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           // Top section
-          Container(
-            height: 100.0,
-            padding: EdgeInsets.only(bottom: 15.0),
-            color: Colors.yellow[300],
-          ),
+          topSection,
 
           // Middle expanded
-          Expanded(
-              child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                Expanded(child: Container(color: Colors.green[300])),
-                Container(
-                  width: 100.0,
-                  color: Colors.red[300],
-                )
-              ])),
+          middleSection,
 
           // Bottom Section
-          Container(height: 80.0, color: Colors.blue[300]),
+          bottomSection,
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'usr',
+      title: 'uex',
       home: Home(),
     );
   }
