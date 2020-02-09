0a02f42b49605aab96de1b823875f29083abcd49
sitais <daniel.levy@sitais.com>
2020-02-09 13:04:45 +0000

    Widgets
    Converted sections to, and added widgets

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-/// Empty page widget fo developers that want to follow along with the tutorial
-/// on breaking down and implementing the Tik Tok UI (BLOG_LINK)
+import 'package:uex/widgets/video_description.dart';
+import 'package:uex/widgets/actions_toolbar.dart';
+import 'package:uex/widgets/bottom_toolbar.dart';
+
 class Home extends StatelessWidget {
   Widget get topSection => Container(
         height: 100.0,
@@ -9,54 +11,54 @@ class Home extends StatelessWidget {
         color: Colors.yellow[300],
       );
 
-  Widget get videoDescription => Expanded(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: EdgeInsets.only(top: 10)),
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: EdgeInsets.only(top: 10)),
-              Container(
-                  height: 10.0,
-                  color: Colors.green[300],
-                  margin: EdgeInsets.only(top: 10))
-            ]),
-      );
+  // Widget get videoDescription => Expanded(
+  //       child: Column(
+  //           mainAxisSize: MainAxisSize.min,
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: <Widget>[
+  //             Container(
+  //                 height: 10.0,
+  //                 color: Colors.green[300],
+  //                 margin: EdgeInsets.only(top: 10)),
+  //             Container(
+  //                 height: 10.0,
+  //                 color: Colors.green[300],
+  //                 margin: EdgeInsets.only(top: 10)),
+  //             Container(
+  //                 height: 10.0,
+  //                 color: Colors.green[300],
+  //                 margin: EdgeInsets.only(top: 10))
+  //           ]),
+  //     );
 
-  Widget get actionsToolbar => Container(
-        width: 100.0,
-        color: Colors.red[300],
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: List<Widget>.generate(
-              5,
-              (index) => Container(
-                  width: 60,
-                  height: 60,
-                  color: Colors.blue[300],
-                  margin: EdgeInsets.only(top: 20.0))),
-        ),
-      );
+  // Widget get actionsToolbar => Container(
+  //       width: 100.0,
+  //       color: Colors.red[300],
+  //       child: Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: List<Widget>.generate(
+  //             5,
+  //             (index) => Container(
+  //                 width: 60,
+  //                 height: 60,
+  //                 color: Colors.blue[300],
+  //                 margin: EdgeInsets.only(top: 20.0))),
+  //       ),
+  //     );
 
   Widget get middleSection => Expanded(
       child: Row(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[videoDescription, actionsToolbar]));
+          children: <Widget>[VideoDescription(), ActionsToolbar()]));
 
-  Widget get bottomSection => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: List<Widget>.generate(
-            5,
-            (index) => Container(
-                width: 40.0, height: 40.0, color: Colors.purple[300])),
-      );
+  // Widget get bottomSection => Row(
+  //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //       children: List<Widget>.generate(
+  //           5,
+  //           (index) => Container(
+  //               width: 40.0, height: 40.0, color: Colors.purple[300])),
+  //     );
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +72,7 @@ class Home extends StatelessWidget {
           middleSection,
 
           // Bottom Section
-          bottomSection,
+          BottomToolbar(),
         ],
       ),
     );
