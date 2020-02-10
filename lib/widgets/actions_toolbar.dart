48b3100d7cdfbfc0d67e4d3e507167712e1c6437
sitais <daniel.levy@sitais.com>
2020-02-10 09:40:22 +0000

    Added TikTokIcons
    changed social widgets for generated widgets

--- lib/widgets/actions_toolbar.dart
+++ lib/widgets/actions_toolbar.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:uex/tik_tok_icons_icons.dart';
+
 class ActionsToolbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 100.0,
-      color: Colors.red[300],
+      padding: EdgeInsets.only(right: 10.0),
       child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: List<Widget>.generate(
-            6,
-            (index) => Container(
-                width: 60,
-                height: 60,
-                color: Colors.blue[300],
-                margin: EdgeInsets.only(top: 20.0))),
-      ),
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            _getSocialAction(icon: TikTokIcons.heart, title: '3.2m'),
+            _getSocialAction(icon: TikTokIcons.chat_bubble, title: '16.4k'),
+            _getSocialAction(icon: TikTokIcons.reply, title: 'Share'),
+          ]),
     );
   }
+}
+
+Widget _getSocialAction({String title, IconData icon}) {
+  return Container(
+      margin: EdgeInsets.only(top: 15.0),
+      width: 60.0,
+      height: 60.0,
+      child: Column(children: [
+        Icon(icon, size: 35.0, color: Colors.grey[300]),
+        Padding(
+          padding: EdgeInsets.only(top: 2.0),
+          child: Text(title, style: TextStyle(fontSize: 12.0)),
+        )
+      ]));
 }
